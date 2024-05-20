417d5f5d9436f07a78de4753630a623fae7ca4de
morb1212 <156122436+morb1212@users.noreply.github.com>
2024-05-20 16:30:07 +0300

    Update test.c

--- test.c
+++ test.c
@@ -9,7 +9,7 @@ int main() {
     char choice[10];
     char file[100];
 
-    // Compression with text file input
+    // Compression
     strcpy(choice, "-c");
     strcpy(file, "input.txt");
     FILE* fp = fopen(file, "w");
@@ -19,29 +19,22 @@ int main() {
     }
     fprintf(fp, "123456789");
     fclose(fp);
-    system("echo 'Before compression:' && cat input.txt");
+    printf("Performing compression with text file input...\n");
     system("./compress -c input.txt");
-    system("echo 'After compression:' && cat input.bin");
-    system("./compress -d input.bin");
-    system("echo 'After decompression:' && cat input.txt");
-    printf("\n");
 
-    // Compression with binary file input
-    strcpy(choice, "-c");
+    // Decompression
+    strcpy(choice, "-d");
     strcpy(file, "input.bin");
     fp = fopen(file, "wb");
     if (fp == NULL) {
         printf("Error opening file for writing.\n");
         return 1;
     }
-    char content[] = "123456789";
+    char content[] = "111110000111100100010110111100010011101100101";
     fwrite(content, sizeof(char), strlen(content), fp);
     fclose(fp);
-    system("echo 'Before compression:' && hexdump -C input.bin");
-    system("./compress -c input.bin");
-    system("echo 'After compression:' && hexdump -C input.bin");
+    printf("\nPerforming decompression with binary file input...\n");
     system("./compress -d input.bin");
-    system("echo 'After decompression:' && hexdump -C input.bin");
 
     return 0;
 }
